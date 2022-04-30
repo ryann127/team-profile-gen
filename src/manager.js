1d408a2d6b713f12aa1c526b1b3cfbb801b1d3ed
ryann127 <ryann.goldberg127@gmail.com>
2022-04-30 14:33:28 -0600

    tidied up some HTML. Uploading current sample HTML to dist folder

--- src/manager.js
+++ src/manager.js
@@ -9,7 +9,7 @@ function renderManager(manager){
                         <p><span class="font-weight-bold">ID: </span> ${manager.id}</p>
                         <p><span class="font-weight-bold">Email: </span><a id="link"
                                 name="email" href="mailto:${manager.email}"> ${manager.email}</a></p>
-                        <p><span class="font-weight-bold">Office number: </span> ${manager.officeNumber}</p>
+                        <p><span class="font-weight-bold">Office Number: </span> ${manager.officeNumber}</p>
                     </div>
                 </div>
     `
